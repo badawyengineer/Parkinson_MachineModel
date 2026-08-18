2314eb9735ff28612313e25068e06db7dd531e6d
Abdelrahman Badawy <YOUR_GITHUB_EMAIL@example.com>
2026-08-19 02:28:19 +0300

    fix: make CI tests environment independent

--- tests/test_model.py
+++ tests/test_model.py
@@ -10,11 +10,31 @@ MODEL_PATH = (
     / "parkinson_model.joblib"
 )
 
-DATA_PATH = Path(
-    "/home/badawy/.cache/kagglehub/datasets/"
-    "debasisdotcom/parkinson-disease-detection/"
-    "versions/2/Parkinsson disease.csv"
-)
+
+SAMPLE = {
+    "MDVP:Fo(Hz)": 119.992,
+    "MDVP:Fhi(Hz)": 157.302,
+    "MDVP:Flo(Hz)": 74.997,
+    "MDVP:Jitter(%)": 0.00784,
+    "MDVP:Jitter(Abs)": 0.00007,
+    "MDVP:RAP": 0.00370,
+    "MDVP:PPQ": 0.00554,
+    "Jitter:DDP": 0.01109,
+    "MDVP:Shimmer": 0.04374,
+    "MDVP:Shimmer(dB)": 0.426,
+    "Shimmer:APQ3": 0.02182,
+    "Shimmer:APQ5": 0.03130,
+    "MDVP:APQ": 0.02971,
+    "Shimmer:DDA": 0.06545,
+    "NHR": 0.02211,
+    "HNR": 21.033,
+    "RPDE": 0.414783,
+    "DFA": 0.815285,
+    "spread1": -4.813031,
+    "spread2": -0.137177,
+    "D2": 3.413649,
+    "PPE": 0.284654,
+}
 
 
 def load_model():
@@ -22,14 +42,7 @@ def load_model():
 
 
 def load_sample():
-    df = pd.read_csv(DATA_PATH)
-
-    X = df.drop(
-        columns=["name", "status"],
-        errors="ignore",
-    )
-
-    return X.iloc[[0]]
+    return pd.DataFrame([SAMPLE])
 
 
 def test_model_artifact_exists():
